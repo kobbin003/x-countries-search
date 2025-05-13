4ede1c1a3f73c80136cc91ad903cc4e60cef59e4
kobbin003 <dkfeto@gmail.com>
2025-05-13 12:20:14 +0530

    xsearchCountries done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [flagLists, setFlagLists] = useState([]);
+	const [searchQuery, setSearchQuery] = useState("");
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const handleOnChange = (e) => {
+		const val = e.target.value;
+		setSearchQuery(val);
+	};
+
+	const filteredFlags = searchQuery
+		? flagLists.filter(({ name }) => name.toLowerCase().includes(searchQuery))
+		: flagLists;
+
+	useEffect(() => {
+		const url = "https://xcountries-backend.azurewebsites.net/all";
+		axios
+			.get(url)
+			.then((res) => setFlagLists(res.data))
+			.catch(() => {
+				console.error("Error fetching data: ");
+			});
+	}, []);
+
+	return (
+		<div>
+			<div className="search-container">
+				<input
+					type="search"
+					name="search"
+					id="search"
+					className="search"
+					onChange={handleOnChange}
+				/>
+			</div>
+			<div className="flag-grid">
+				{filteredFlags.length > 0 ? (
+					filteredFlags.map(({ name, flag }) => (
+						<div className="flag-grid-item">
+							<img src={flag} alt={name} />
+							<p>{name}</p>
+						</div>
+					))
+				) : (
+					<></>
+				)}
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
